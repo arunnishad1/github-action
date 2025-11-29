cd2bd7687fbd533b34c8fc854cabb1edb700b11e
NISHAD-5123 <arunnishad94136@gmail.com>
2025-11-30 00:08:49 +0530

    changes

--- subtraction.py
+++ subtraction.py
@@ -4,7 +4,7 @@ def subtraction(num1, num2):
 
 def test_subtract():
     assert subtraction(5, 3) == 2
-    assert subtraction(10, ) == -5
+    assert subtraction(10, 4) == -5
     assert subtraction(0, 0) == 0
 
 
